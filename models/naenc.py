30f828c3f43f4a8594126c3625aa1e5fcd8f1428
Mehdi Mirza <memirzamo@gmail.com>
2012-10-10 14:16:22 -0400

    reorganized

---
+++ models/naenc.py
@@ -0,0 +1,58 @@
+from theano import tensor
+from pylearn2.autoencoder import Autoencoder
+
+
+class NoisyAutoencoder(Autoencoder):
+
+    def __init__(self, input_corruptor, hidden_corruptor,
+            nvis, nhid, act_enc, act_dec,
+            tied_weights = False, irange=1e-3, rng=9001):
+
+        super(NoisyAutoencoder, self).__init__(
+        nvis,
+        nhid,
+        act_enc,
+        act_dec,
+        tied_weights,
+        irange,
+        rng)
+
+        self.input_corruptor = input_corruptor
+        self.hidden_corruptor = hidden_corruptor
+
+    def _hidden_activation(self, x):
+
+        hidden = super(NoisyAutoencoder, self)._hidden_activation(x)
+        return self.hidden_corruptor(hidden)
+
+    def test_encode(self, inputs):
+
+        if isinstance(inputs, tensor.Variable):
+            return super(NoisyAutoencoder, self)._hidden_activation(inputs)
+        else:
+            return [self.encode(v) for v in inputs]
+
+    def reconstruct(self, inputs):
+        """
+        Reconstruct the inputs after corrupting and mapping through the
+        encoder and decoder.
+
+        Parameters
+        ----------
+        inputs : tensor_like or list of tensor_likes
+            Theano symbolic (or list thereof) representing the input
+            minibatch(es) to be corrupted and reconstructed. Assumed to be
+            2-tensors, with the first dimension indexing training examples and
+            the second indexing data dimensions.
+
+        Returns
+        -------
+        reconstructed : tensor_like or list of tensor_like
+            Theano symbolic (or list thereof) representing the corresponding
+            reconstructed minibatch(es) after corruption and encoding/decoding.
+        """
+        corrupted = self.input_corruptor(inputs)
+        return super(NoisyAutoencoder, self).reconstruct(corrupted)
+
+    def test_reconstruct(self, inputs):
+        return super(NoisyAutoencoder, self).reconstruct(inputs)
--- util/config.py
+++
@@ -1,3 +0,0 @@
-global DATA_PATH
-#DATA_PATH = "/RQexec/mirzameh/data/"
-DATA_PATH = "/data/lisatmp/mirzamom/data/"
