2aa22b8ed5101acbaa9384e902a7149ed042ff9c
Mehdi Mirza <memirzamo@gmail.com>
2013-02-01 16:25:21 -0500

    clean up

---
+++ scripts/train/train_supervised_pylearn.py
@@ -0,0 +1,173 @@
+import os
+import argparse
+
+from pylearn2.config import yaml_parse
+from my_utils.config import get_data_path, get_result_path
+from jobman.tools import DD
+
+DATA_PATH = get_data_path()
+RESULT_PATH = get_result_path()
+
+# convolution
+convolution_yaml_string = """
+!obj:pylearn2.train.Train {
+    dataset: &train %(train_set)s,
+    model: !obj:noisy_encoder.models.conv.LeNetLearner {
+        conv_layers: %(conv_layers)s,
+        batch_size : %(batch_size)i,
+        mlp_act : %(mlp_act)s,
+        mlp_input_corruption_levels : %(mlp_input_corruption_levels)s,
+        mlp_hidden_corruption_levels : %(mlp_hidden_corruption_levels)s,
+        mlp_nunits : %(mlp_nunits)s,
+        n_outs : %(n_outs)i,
+        irange : %(irange)f,
+        bias_init : %(bias_init)f,
+        random_filters : %(random_filters)s,
+    },
+    algorithm: !obj:pylearn2.training_algorithms.sgd.SGD {
+        batch_size : %(batch_size)i,
+        learning_rate : %(init_learning_rate)f,
+        init_momentum : %(init_momentum)f,
+        monitoring_dataset : {
+            valid : %(valid_set)s,
+            test : %(test_set)s},
+        cost : !obj:pylearn2.costs.cost.SumOfCosts { costs : [
+                !obj:pylearn2.costs.cost.MethodCost {
+                    method : 'cost',
+                    supervised : 1}]
+        },
+        termination_criterion : !obj:pylearn2.termination_criteria.MonitorBased {
+            channel_name: "valid_misclass",
+            prop_decrease: 0.,
+            N: 10
+        },
+    },
+    extensions:
+        [ !obj:pylearn2.train_extensions.best_params.MonitorBasedSaveBest {
+            channel_name: 'valid_misclass',
+            save_path: "convolutional_network_best.pkl"
+        }, !obj:pylearn2.training_algorithms.sgd.MomentumAdjustor {
+                start: 1,
+                saturate: 10,
+                final_momentum: .99
+        }
+        ]
+    }
+"""
+
+def experiment(state, channel):
+    # update base yaml config with jobman commands
+    yaml_string = state.yaml_string % (state)
+
+    # save .yaml file if it is jobman job
+    if channel != None:
+        with open('model.yaml', 'w') as fp:
+            fp.write(yaml_string)
+
+    # now run yaml file with default train.py script
+    train_obj = yaml_parse.load(yaml_string)
+    train_obj.main_loop()
+
+    try:
+        state.final_cost = float(train_obj.algorithm.monitor.channels['sgd_cost'].val_record[-1])
+    except TypeError:
+        pass
+
+    return channel.COMPLETE
+
+
+def tfd_conv_experiment():
+
+    state = DD()
+
+    # train params
+    state.dataset = 'tfd'
+    state.fold = 0
+    #state.train_set = "!obj:pylearn2.datasets.tfd.TFD {which_set: 'train', one_hot: 1}"
+    #state.valid_set = "!obj:pylearn2.datasets.tfd.TFD {which_set: 'valid', one_hot: 1}"
+    #state.test_set = "!obj:pylearn2.datasets.tfd.TFD {which_set: 'test', one_hot: 1}"
+    state.train_set = "!pkl: " + os.path.join(DATA_PATH, "faces/TFD/pylearn2/{}/train.pkl".format(state.fold))
+    state.valid_set = "!pkl: " + os.path.join(DATA_PATH, "faces/TFD/pylearn2/{}/test.pkl".format(state.fold))
+    state.test_set = "!pkl: " + os.path.join(DATA_PATH, "faces/TFD/pylearn2/{}/valid.pkl".format(state.fold))
+    state.init_learning_rate = 0.0001
+    state.init_momentum = 0.5
+    state.batch_size = 1
+    state.save_name = os.path.join(RESULT_PATH, "naenc/tfd/tfd_gpu.pkl")
+    state.coeffs = {'w_l1' : 0.0, 'w_l2' : 0e-5}
+    # model params
+    state.model = 'conv'
+    state.conv_layers = [
+             {'name' : 'LocalResponseNormalize',
+                    'params' : {'image_shape' : [48, 48],
+                            'batch_size' : state.batch_size,
+                            'num_channels' : 1,
+                            'n' : 4,
+                            'k' : 1,
+                            'alpha' : 0e-04,
+                            'beta' : 0.75}},
+                {'name' : 'Convolution',
+                    'params' : {'image_shape' : [48, 48],
+                            'kernel_shape' : [7, 7],
+                            'num_channels' : 1,
+                            'num_channels_output' : 64,
+                            'batch_size' : state.batch_size,
+                            'act_enc' : 'rectifier',}},
+                {'name' : 'StochasticMaxPool',
+                    'params' : {'image_shape' : None,
+                        'num_channels' : None,
+                        'pool_shape' : [3, 3],
+                        'pool_stride' : [2, 2]}},
+                {'name' : 'LocalResponseNormalize',
+                    'params' : {'image_shape' : None,
+                            'batch_size' : state.batch_size,
+                            'num_channels' : None,
+                            'n' : 4,
+                            'k' : 1,
+                            'alpha' : 0e-04,
+                            'beta' : 0.75
+                            }},
+                {'name' : 'Convolution',
+                    'params' : {'image_shape' : None,
+                            'kernel_shape' : [5, 5],
+                            'num_channels' : None,
+                            'num_channels_output' : 64,
+                            'batch_size' : state.batch_size,
+                            'act_enc' : 'rectifier',}},
+                {'name' : 'StochasticMaxPool',
+                    'params' : {'image_shape' : None,
+                        'num_channels' : None,
+                        'pool_shape' : [3, 3],
+                        'pool_stride' : [2, 2]}},
+                {'name' : 'LocalResponseNormalize',
+                    'params' : {'image_shape' : None,
+                            'batch_size' : state.batch_size,
+                            'num_channels' :None ,
+                            'n' : 4,
+                            'k' : 1,
+                            'alpha' : 0e-04,
+                            'beta' : 0.75
+                            }}]
+    state.mlp_act = "rectifier"
+    state.mlp_input_corruption_levels = [0.0, 0.0]
+    state.mlp_hidden_corruption_levels = [0.5, 0.5]
+    state.mlp_nunits = [1200]
+    state.n_outs = 7
+    state.bias_init = 0.1
+    state.irange = 0.1
+    state.random_filters = False
+
+    state.yaml_string = convolution_yaml_string
+
+    experiment(state, None)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description = 'noisy AE trainer')
+    parser.add_argument('-t', '--task', choices = ['tfd_conv'], required = True)
+    args = parser.parse_args()
+
+    if args.task == 'tfd_conv':
+        tfd_conv_experiment()
+    else:
+        raise ValueError("Wrong task optipns {}".format(args.task))
+
