cecc28ffb67948b7feaa874b4adc04ca645965c5
Mehdi Mirza <memirzamo@gmail.com>
2013-05-27 16:38:36 -0400

    submit dataset added

--- scripts/nips2013/submit.py
+++ scripts/nips2013/submit.py
@@ -35,15 +35,47 @@ def norb():
     db.createView(TABLE_NAME + '_view')
     print "{} jobs submitted".format(ind)
 
+def convex():
+    state = DD()
+    with open('exp/convex.yaml') as ymtmp:
+        state.yaml_string = ymtmp.read()
+
+    state.db = 'convex'
+    state.layer0_dim = 500
+    state.layer1_dim = 1000
+    state.layer2_dim = 2000
+    state.learning_rate = 2.
+    state.decay_factor = 0.066484
+    state.lr_saturate = 217
+    state.m_saturate = 2
+    state.final_momentum = 0.802294
+    state.save_path = './'
+    state.save = "/data/lisatmp/mirzamom/exp/pdbm/convex/"
+
+    ind = 0
+    TABLE_NAME = "pdbm_convex"
+    db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
+    for lr in [100, 10, 1, 0.1]:
+        for dec in [0.1, 0.01]:
+            state.learning_rate = lr
+            state.decay_factor = dec
+            experiment(state, None)
+            #sql.insert_job(experiment, flatten(state), db)
+            ind += 1
+
+    db.createView(TABLE_NAME + '_view')
+    print "{} jobs submitted".format(ind)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description = 'job submitter')
-    parser.add_argument('-t', '--task', choices = ['norb'])
+    parser.add_argument('-t', '--task', choices = ['norb', 'convex'])
     args = parser.parse_args()
 
     if args.task == 'norb':
         norb()
+    elif args.task == 'convex':
+        convex()
     else:
         raise ValueError("Wrong task optipns {}".fromat(args.task))
 
