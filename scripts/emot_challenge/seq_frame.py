1a244b5f352b8ed63ba11eca7b597c4ef88587e4
Mehdi Mirza <memirzamo@gmail.com>
2013-07-10 17:16:57 -0400

    seq_frame added

---
+++ scripts/emot_challenge/seq_frame.py
@@ -0,0 +1,86 @@
+"""
+Get sequence classification reuslts based on single based classifiers
+"""
+
+import numpy
+from pylearn2.utils import py_integer_types
+from pylearn2.utils import serial
+from pylearn2.config import yaml_parse
+import theano.tensor as T
+from theano import function
+import sys
+from emotiw.common.datasets.faces.afew2_facetubes import AFEW2FaceTubes
+import ipdb
+
+
+
+def get_classifier(model):
+
+
+    X = model.get_input_space().make_batch_theano()
+    X.name = 'X'
+
+    y = model.fprop(X)
+    y.name = 'y'
+
+    return function([X],y)
+
+
+def accuracy(predictions, target):
+
+    mx = numpy.argmax(predictions, axis=1)
+    mask = numpy.zeros(predictions.shape)
+    for i in range(mask.shape[0]):
+        mask[i, mx[i]] = 1.
+    predictions = mask * predictions
+    y_hat = numpy.argmax(predictions.sum(axis=0))
+    return int(y_hat != numpy.argmax(target))
+
+if __name__ == "__main__":
+
+    data = AFEW2FaceTubes(which_set='valid', one_hot=True, preproc=['smooth'], size=[48,48])
+    _, model_path = sys.argv
+    model = serial.load(model_path)
+    batch_size = model.batch_size
+    classifier = get_classifier(model)
+
+    garbage = numpy.ones((1, 48, 48, 100)).astype('float32')
+    # organize axis
+    data_axes = data.data_specs[0].components[0].axes
+    if 't' in data_axes:
+        data_axes = [axis for axis in data_axes if axis not in ['t']]
+    model_axes = model.input_space.axes
+    if model_axes != ('c', 0, 1, 'b'):
+        raise ValueError("Model axis is not supoorted")
+
+    misclass = []
+    for feature, target in zip(data.features, data.targets):
+        feature = feature / 255.
+        feature = feature.astype('float32')
+        if data_axes != model_axes:
+            feature = feature.transpose(*[data_axes.index(axis) for axis in model_axes])
+
+        num_samples = feature.shape[3]
+        predictions = []
+        for i in range(num_samples / batch_size):
+            # TODO FIX ME, after grayscale
+            predictions.append(classifier(feature[0,:,:,i*batch_size:(i+1)*batch_size][numpy.newaxis,:,:,:]))
+
+        # for modulo we padd with garbage
+        if batch_size > num_samples:
+            modulo = batch_size - num_samples
+        else:
+            modulo = num_samples % batch_size
+
+        if modulo != 0:
+            # TODO FIX ME, after grayscale
+            shape = [1, feature.shape[1], feature.shape[2], modulo]
+            padding = numpy.ones((shape)).astype('float32')
+            # TODO FIX ME, after grayscale
+            feature = numpy.concatenate((feature[0,:,:,(num_samples/batch_size) * batch_size:][numpy.newaxis,:,:,:], padding), axis = 3)
+            predictions.append(classifier(feature)[:batch_size - modulo])
+        predictions = numpy.concatenate(predictions, axis=0)
+        misclass.append(accuracy(predictions, target))
+
+
+    print numpy.sum(misclass) / float(len(data.features))
--- scripts/emot_challenge/submit.py
+++ scripts/emot_challenge/submit.py
@@ -33,6 +33,31 @@ def frame():
     db.createView(TABLE_NAME + '_view')
     print "{} jobs submitted".format(ind)
 
+def seq3d():
+    state = DD()
+    with open('exp/frame_3dconv.yaml') as ymtmp:
+        state.yaml_string = ymtmp.read()
+
+    state.db = 'afew'
+    state.learning_rate = 0.01
+    state.h0_num_channels = 64
+    state.h1_num_channels = 128
+    state.last_ndim = 400
+    #state.save_path = './'
+    state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/challenge/frame/")
+
+    ind = 0
+    TABLE_NAME = "challenge_frame_3dseq"
+    db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
+    for lr in [0.1]:
+        state.learning_rate = lr
+        experiment(state, None)
+        #sql.insert_job(experiment, flatten(state), db)
+        ind += 1
+
+    db.createView(TABLE_NAME + '_view')
+    print  "{} jobs submitted".format(ind)
+
 def crf():
     state = DD()
     with open('exp/crf_conv.yaml') as ymtmp:
@@ -63,13 +88,15 @@ def crf():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description = 'job submitter')
-    parser.add_argument('-t', '--task', choices = ['frame', 'crf'])
+    parser.add_argument('-t', '--task', choices = ['frame', 'crf', 'seq3d'])
     args = parser.parse_args()
 
     if args.task == 'frame':
         frame()
     elif args.task == 'crf':
         crf()
+    elif args.task == 'seq3d':
+        seq3d()
     else:
         raise ValueError("Wrong task optipns {}".fromat(args.task))
 
