f4344adf4fc0f99e76579fd53ef4aeba6065a623
Mehdi Mirza <memirzamo@gmail.com>
2013-08-07 16:05:25 -0400

    cifar weighted maxout added

--- scripts/gating/submit.py
+++ scripts/gating/submit.py
@@ -8,9 +8,9 @@ from pylearn2.utils.string_utils import preprocess
 
 DATA_PATH = get_data_path()
 
-def kl(submit = False):
+def softmax(submit = False):
     state = DD()
-    with open('exp/kl.yaml') as ymtmp:
+    with open('exp/sigmoid.yaml') as ymtmp:
         state.yaml_string = ymtmp.read()
 
     state.db = 'mnist'
@@ -18,6 +18,7 @@ def kl(submit = False):
     state.h0_pieces = 10
     state.h1_units = 1000
     state.h1_pieces = 10
+    sate.selection_type = 'one_hot'
     state.sparsity_ratio_0 = 0.1
     state.sparsity_ratio_1 = 0.1
     state.sparsity_momentum_0 = 0.9
@@ -112,21 +113,62 @@ def normal(submit):
     db.createView(TABLE_NAME + '_view')
     print "{} jobs submitted".format(ind)
 
+def stochastic(submit = False):
+    state = DD()
+    with open('exp/softmax.yaml') as ymtmp:
+        state.yaml_string = ymtmp.read()
+
+    state.db = 'mnist'
+    state.h0_units = 1000
+    state.h0_pieces = 10
+    state.h1_units = 1000
+    state.h1_pieces = 10
+    state.selection_type = 'stochastic'
+    state.sparsity_ratio_0 = 0.1
+    state.sparsity_ratio_1 = 0.1
+    state.sparsity_momentum_0 = 0.9
+    state.sparsity_momentum_1 = 0.9
+    state.sparsity_penalty = 0.1
+    state.learning_rate = 0.1
+    state.decay_factor = 0.001
+    state.save_path = './'
+    #state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/gating/sig/")
+
+    ind = 0
+    TABLE_NAME = "gate_kl_sm"
+    db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
+    for lr in [0.1]:
+        for s0, s1 in zip ([0.1], [0.1]):
+            for sp in [0.1]:
+                state.learning_rate = lr
+                state.sparsity_ratio_0 = s0
+                state.sparsity_ratio_1 = s1
+                state.sparsity_penalty = sp
+                if submit:
+                    sql.insert_job(experiment, flatten(state), db)
+                else:
+                    experiment(state, None)
+                ind += 1
+
+    db.createView(TABLE_NAME + '_view')
+    print "{} jobs submitted".format(ind)
 
 
 if __name__ == "__main__":
 
     parser  = argparse.ArgumentParser(description = 'job submitter')
-    parser.add_argument('-t', '--task', choices = ['kl', 'kl_sig', 'normal'])
+    parser.add_argument('-t', '--task', choices = ['softmax', 'kl_sig', 'normal', 'stochastic'])
     parser.add_argument('-s', '--submit', default = False, action='store_true')
     args = parser.parse_args()
 
-    if args.task == 'kl':
-        kl(args.submit)
+    if args.task == 'softmax':
+        softmax(args.submit)
     elif args.task == 'kl_sig':
         kl_sig(args.submit)
     elif args.task == 'normal':
         normal(args.submit)
+    elif args.task == 'stochastic':
+        stochastic(args.submit)
     else:
         raise ValueError("Wrong task optipns {}".fromat(args.task))
 
