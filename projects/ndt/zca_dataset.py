9554f2f616855f999dfe3755ca4457483f65abc6
Mehdi Mirza <memirzamo@gmail.com>
2013-11-11 16:58:54 -0500

    cifar_bin fixed

--- projects/ndt/zca_dataset.py
+++ projects/ndt/zca_dataset.py
@@ -1,4 +1,5 @@
-from pylearn2.datasets.mnist import MNIST
+import numpy as np
+from pylearn2.datasets.zca_dataset import ZCA_Dataset
 
 
 class IndexedZCA_Dataset(ZCA_Dataset):
@@ -11,18 +12,16 @@ class IndexedZCA_Dataset(ZCA_Dataset):
             stop = None,
             axes = ['b', 0, 1, 'c']):
 
-        super(IndexedZCA_Dataset, self.__init__(preprocessed_dataset=preprocessed_dataset,
+        super(IndexedZCA_Dataset, self).__init__(preprocessed_dataset=preprocessed_dataset,
                                         preprocessor=preprocessor,
                                         convert_to_one_hot=convert_to_one_hot,
                                         start=start,
                                         stop=stop,
-                                        axes=axes))
+                                        axes=axes)
 
         if self.X is not None:
             self.X = self.X[indexes]
             self.y=self.y[indexes]
-
-
 
 
 class ZCA_Dataset_BIN(ZCA_Dataset):
@@ -35,19 +34,16 @@ class ZCA_Dataset_BIN(ZCA_Dataset):
             stop = None,
             axes = ['b', 0, 1, 'c']):
 
-        super(IndexedZCA_Dataset, self.__init__(preprocessed_dataset=preprocessed_dataset,
+        super(ZCA_Dataset_BIN, self).__init__(preprocessed_dataset=preprocessed_dataset,
                                         preprocessor=preprocessor,
                                         convert_to_one_hot=convert_to_one_hot,
                                         start=start,
                                         stop=stop,
-                                        axes=axes))
+                                        axes=axes)
 
         if self.y is not None:
             new_y = np.zeros((self.y.shape[0], 2))
-            for item in self.y:
-                new_y[labels[np.argmax(item)]] = 1
+            for i in xrange(len(self.y)):
+                new_y[i, labels[np.argmax(self.y[i])]] = 1
 
-
-
-        import ipdb
-        ipdb.set_trace()
+            self.y = new_y
