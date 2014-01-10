6024196a612e7c365b8bbbecc988ac22cf67326c
Mehdi Mirza <memirzamo@gmail.com>
2014-01-10 17:28:05 -0500

    locally connected layer

--- projects/tiled/__init__.py
+++ projects/tiled/__init__.py
@@ -3,61 +3,33 @@ from theano import tensor as T
 from theano.printing import Print
 from theano.compat.python2x import OrderedDict
 from pylearn2.models.mlp import Linear
+from pylearn2.models.mlp import ConvRectifiedLinear
 from pylearn2.models.mlp import Softmax
+from pylearn2.models.mlp import max_pool
+from pylearn2.models.mlp import mean_pool
+from pylearn2.models.maxout import MaxoutLocalC01B
 from pylearn2.linear.matrixmul import MatrixMul
 from pylearn2.linear import local_c01b
 from pylearn2.space import VectorSpace
+from pylearn2.space import Conv2DSpace
 from pylearn2.utils import sharedX
 from pylearn2.format.target_format import OneHotFormatter
 
-class LocalLinear(Linear):
+class LocalLinearVector(Linear):
 
-    def __init__(self,
-                 dim,
-                 kernel_shape,
-                 layer_name,
-                 kernel_stride = (1, 1),
-                 pad = 0,
-                 partial_sum = 1,
-                 irange = None,
-                 istdev = None,
-                 sparse_init = None,
-                 sparse_stdev = 1.,
-                 include_prob = 1.0,
-                 init_bias = 0.,
-                 W_lr_scale = None,
-                 b_lr_scale = None,
-                 mask_weights = None,
-                 max_row_norm = None,
-                 max_col_norm = None,
-                 softmax_columns = False,
-                 copy_input = 0,
-                 use_abs_loss = False,
-                 use_bias = True):
-
-        self.__dict__.update(locals())
-        del self.self
-        super(LocalLinear, self).__init__(dim = dim,
-                 layer_name = layer_name,
-                 irange = irange,
-                 istdev = istdev,
-                 sparse_init = sparse_init,
-                 sparse_stdev = sparse_stdev,
-                 include_prob = include_prob,
-                 init_bias = init_bias,
-                 W_lr_scale = W_lr_scale,
-                 b_lr_scale = b_lr_scale,
-                 mask_weights = mask_weights,
-                 max_row_norm = max_row_norm,
-                 max_col_norm = max_col_norm,
-                 softmax_columns = softmax_columns,
-                 copy_input = copy_input,
-                 use_abs_loss = use_abs_loss,
-                 use_bias = use_bias)
+    def __init__(self, dim, layer_name, kernel_shape, kernel_stride = (1, 1), **kwargs):
+        super(LocalLinear, self).__init__(dim, layer_name, **kwargs)
+        self.kernel_shape = kernel_shape
+        self.kernel_stride = kernel_stride
 
     def set_input_space(self, space):
 
         self.input_space = space
+
+        # find the image dimension
+        image_shape = int(np.sqrt(self.dim))
+        assert image_shape ** 2 == self.dim
+        self.image_shape = (image_shape, image_shape)
 
         if isinstance(space, Conv2DSpace):
             self.requires_reformat = False
@@ -68,14 +40,10 @@ class LocalLinear(Linear):
                                             channels = 1,
                                             axes = ('c', 0, 1, 'b'))
 
-        self.output_space = VectorSpace(self.dim + self.copy_input * self.input_dim)
+        self.output_space = VectorSpace(self.dim)
 
         rng = self.mlp.rng
 
-        # find the image dimension
-        image_shape = int(np.sqrt(self.input_space.dim))
-        assert image_shape ** 2 == self.input_space.dim
-        self.image_shape = (image_shape, image_shape)
 
         self.transformer = local_c01b.make_random_local(
                 input_groups = 1,
@@ -118,7 +86,138 @@ class LocalLinear(Linear):
 
         return z
 
+class LocalRectified(ConvRectifiedLinear):
 
+    def set_input_space(self, space):
+        """
+        .. todo::
+
+            WRITEME
+
+        Notes
+        -----
+        This resets parameters!
+        """
+
+        self.input_space = space
+        rng = self.mlp.rng
+
+        if not isinstance(self.input_space, Conv2DSpace):
+            raise TypeError("The input to ths layer should be a Conv2DSpace"
+                    " but layer " + self.layer_name + " got " + str(type(self.input_space)))
+
+        if self.border_mode == 'valid':
+            output_shape = [(self.input_space.shape[0] - self.kernel_shape[0]) / self.kernel_stride[0] + 1,
+                (self.input_space.shape[1] - self.kernel_shape[1]) / self.kernel_stride[1] + 1]
+        elif self.border_mode == 'full':
+            output_shape = [(self.input_space.shape[0] +  self.kernel_shape[0]) / self.kernel_stride[0] - 1,
+                    (self.input_space.shape[1] + self.kernel_shape[1]) / self.kernel_stride_stride[1] - 1]
+
+        self.detector_space = Conv2DSpace(shape=output_shape,
+                num_channels = self.output_channels,
+                axes = ('b', 'c', 0, 1))
+
+        if self.irange is not None:
+            assert self.sparse_init is None
+            # TODO make local_c01b accept space instead of space params
+            self.transformer = local_c01b.make_random_local(
+                input_groups = 1,
+                irange = self.irange,
+                input_axes = self.input_space.axes,
+                image_shape = self.input_space.shape,
+                output_axes = self.detector_space.axes,
+                input_channels = self.input_space.num_channels,
+                output_channels = self.detector_space.num_channels,
+                kernel_shape = self.kernel_shape,
+                kernel_stride = self.kernel_stride,
+                # TODO
+                pad = 0,
+                partial_sum = 1,
+                rng = rng)
+
+
+        elif self.sparse_init is not None:
+            raise NotImplementedError()
+            #self.transformer = conv2d.make_sparse_random_conv2D(
+                    #num_nonzero = self.sparse_init,
+                    #input_space = self.input_space,
+                    #output_space = self.detector_space,
+                    #kernel_shape = self.kernel_shape,
+                    #batch_size = self.mlp.batch_size,
+                    #subsample = self.kernel_stride,
+                    #border_mode = self.border_mode,
+                    #rng = rng)
+
+        W, = self.transformer.get_params()
+        W.name = 'W'
+
+        self.b = sharedX(self.detector_space.get_origin() + self.init_bias)
+        self.b.name = 'b'
+
+        print 'Input shape: ', self.input_space.shape
+        print 'Detector space: ', self.detector_space.shape
+
+
+        assert self.pool_type in ['max', 'mean']
+
+        dummy_batch_size = self.mlp.batch_size
+        if dummy_batch_size is None:
+            dummy_batch_size = 2
+        dummy_detector = sharedX(self.detector_space.get_origin_batch(dummy_batch_size))
+        if self.pool_type == 'max':
+            dummy_p = max_pool(bc01=dummy_detector, pool_shape=self.pool_shape,
+                    pool_stride=self.pool_stride,
+                    image_shape=self.detector_space.shape)
+        elif self.pool_type == 'mean':
+            dummy_p = mean_pool(bc01=dummy_detector, pool_shape=self.pool_shape,
+                    pool_stride=self.pool_stride,
+                    image_shape=self.detector_space.shape)
+        dummy_p = dummy_p.eval()
+        self.output_space = Conv2DSpace(shape=[dummy_p.shape[2], dummy_p.shape[3]],
+                num_channels = self.output_channels, axes = ('b', 'c', 0, 1) )
+
+        print 'Output space: ', self.output_space.shape
+
+
+    def get_filter_norms(self, W = None):
+
+        if W is None:
+            W, = self.transformer.get_params()
+
+        assert W.ndim == 7
+        sq_W = T.sqr(W)
+        norms = T.sqrt(sq_W.sum(axis=(2, 3, 4)))
+        return norms
+
+    def get_monitoring_channels(self):
+        """
+        .. todo::
+
+            WRITEME
+        """
+
+        filter_norms = self.get_filter_norms()
+        return OrderedDict([
+                            ('filter_norms_min'  , filter_norms.min()),
+                            ('filter_norms_mean' , filter_norms.mean()),
+                            ('filter_norms_max'  , filter_norms.max()),
+                            ])
+
+    def censor_updates(self, updates):
+        """
+        .. todo::
+
+            WRITEME
+        """
+
+        if self.max_kernel_norm is not None:
+            W ,= self.transformer.get_params()
+            if W in updates:
+                updated_W = updates[W]
+                updated_norms = self.get_filter_norms(updated_W)
+                desired_norms = T.clip(updated_norms, 0, self.max_kernel_norm)
+                updates[W] = updated_W * (desired_norms / (1e-7 + updated_norms)
+                        ).dimshuffle(0, 1, 'x', 'x', 'x', 2, 3)
 
 class EmbeddingLinear(Linear):
 
@@ -208,7 +307,6 @@ class EmbeddingLinear(Linear):
 
         return z
 
-
 class TransposeEmbeddingLinear(Linear):
 
     def set_input_space(self, space):
@@ -253,9 +351,12 @@ class TransposeEmbeddingLinear(Linear):
                 raise ValueError("Expected mask with shape "+str(expected_shape)+" but got "+str(self.mask_weights.shape))
             self.mask = sharedX(self.mask_weights)
 
-
 class CompactSoftmax(Softmax):
-
+    """
+        This difference with Softmax is:
+            * get the data in 1-D on convert to One_hot on the fly
+            * return perplexity instead of missclass
+    """
     def __init__(self, **kwargs):
 
         super(CompactSoftmax, self).__init__(**kwargs)
@@ -323,4 +424,104 @@ class CompactSoftmax(Softmax):
             rval['perplexity'] = 10 ** (rval['nll'] / np.log(10).astype('float32'))
 
         return rval
+
+
+class MaxoutLocalC01BPoolLess(MaxoutLocalC01B):
+
+     def fprop(self, state_below):
+        """
+        .. todo::
+
+            WRITEME
+        """
+
+        self.input_space.validate(state_below)
+
+        state_below = self.input_space.format_as(state_below, self.desired_space)
+
+        if not hasattr(self, 'input_normalization'):
+            self.input_normalization = None
+
+        if self.input_normalization:
+            state_below = self.input_normalization(state_below)
+
+        # Alex's code requires # input channels to be <= 3 or a multiple of 4
+        # so we add dummy channels if necessary
+        if not hasattr(self, 'dummy_channels'):
+            self.dummy_channels = 0
+        if self.dummy_channels > 0:
+            state_below = T.concatenate((state_below,
+                                         T.zeros_like(state_below[0:self.dummy_channels, :, :, :])),
+                                        axis=0)
+
+        z = self.transformer.lmul(state_below)
+        if not hasattr(self, 'tied_b'):
+            self.tied_b = False
+        if self.tied_b:
+            b = self.b.dimshuffle(0, 'x', 'x', 'x')
+        else:
+            b = self.b.dimshuffle(0, 1, 2, 'x')
+
+
+        z = z + b
+        if self.layer_name is not None:
+            z.name = self.layer_name + '_z'
+
+        self.detector_space.validate(z)
+
+        assert self.detector_space.num_channels % 16 == 0
+
+        if self.output_space.num_channels % 16 == 0:
+            # alex's max pool op only works when the number of channels
+            # is divisible by 16. we can only do the cross-channel pooling
+            # first if the cross-channel pooling preserves that property
+            if self.num_pieces != 1:
+                s = None
+                for i in xrange(self.num_pieces):
+                    t = z[i::self.num_pieces,:,:,:]
+                    if s is None:
+                        s = t
+                    else:
+                        s = T.maximum(s, t)
+                z = s
+
+            if self.detector_normalization:
+                z = self.detector_normalization(z)
+
+            if self.pool_shape is None:
+                p = z
+            else:
+                raise NotImplementedError()
+        else:
+
+            if self.detector_normalization is not None:
+                raise NotImplementedError("We can't normalize the detector "
+                        "layer because the detector layer never exists as a "
+                        "stage of processing in this implementation.")
+            if self.pool_shape is not None:
+                raise NotImplementedError()
+            if self.num_pieces != 1:
+                s = None
+                for i in xrange(self.num_pieces):
+                    t = z[i::self.num_pieces,:,:,:]
+                    if s is None:
+                        s = t
+                    else:
+                        s = T.maximum(s, t)
+                z = s
+            p = z
+
+
+        self.output_space.validate(p)
+
+        if hasattr(self, 'min_zero') and self.min_zero:
+            p = p * (p > 0.)
+
+        if not hasattr(self, 'output_normalization'):
+            self.output_normalization = None
+
+        if self.output_normalization:
+            p = self.output_normalization(p)
+
+        return p
 
