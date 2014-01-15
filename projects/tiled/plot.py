0d545ba34a0c9372fa930ef26023b68105be7c27
Mehdi Mirza <memirzamo@gmail.com>
2014-01-15 13:30:20 -0500

    plot updated

--- projects/tiled/plot.py
+++ projects/tiled/plot.py
@@ -1,8 +1,9 @@
 import os
-import sys
 import gc
+import argparse
 import numpy as np
 from pylearn2.utils import serial
+from pylearn2.config import yaml_parse
 
 import ipdb
 
@@ -16,9 +17,11 @@ def extract(path, save_name):
     data = {'valid' : [], 'test' : [], 'train' : [],
             'epoch' : [], 'l0_params' : [], 'l2_params' : [],
             'l3_params' : [], 'l4_params' : [],
-            'learning_rate' : [],
-            'kernel_shape' : [],
-            'id' : []}
+            'l2_pieces' : [], 'l2_kernel' : [],
+            'l3_pieces' : [], 'l3_units' : [],
+            'learning_rate' : [], 'momentum_saturate' : [],
+            'final_momentum' : [], 'decay_factor' :[],
+            'lr_saturate' : [], 'id' : []}
     fs = os.listdir(path)
     for f in fs:
         print f
@@ -31,18 +34,36 @@ def extract(path, save_name):
         data['l2_params'].append(layer_num_params(model.layers[2]))
         data['l3_params'].append(layer_num_params(model.layers[3]))
         data['l4_params'].append(layer_num_params(model.layers[4]))
+        data['l2_pieces'].append(model.layers[2].num_pieces)
+        data['l2_kernel'].append(model.layers[2].kernel_shape[0])
+        data['l3_pieces'].append(model.layers[3].num_pieces)
+        data['l3_units'].append(model.layers[3].num_units)
 
         gc.collect()
 
-        yaml = open(os.path.join(path, f, 'model.yaml'), 'r').read()
-        data['learning_rate'].append(float(yaml[yaml.index('learning_rate'):].split(',')[0].split(':')[1]))
-        data['kernel_shape'].append(int(yaml[yaml.index('kernel_shape'):].split(',')[0].split(':')[1].split('[')[1]))
+        conf = yaml_parse.load(open(os.path.join(path, f, 'model.yaml'), 'r'))
+        data['learning_rate'].append(conf.algorithm.learning_rate.get_value())
+        data['momentum_saturate'].append(conf.extensions[1].saturate)
+        data['final_momentum'].append(conf.extensions[1].final_momentum)
+        data['lr_saturate'].append(conf.extensions[2].saturate)
+        data['decay_factor'].append(conf.extensions[2].decay_factor)
         data['id'].append(int(f))
-
 
     serial.save("{}.pkl".format(save_name), data)
 
 
+def plot()
+
 if __name__ == "__main__":
+
+
+    parser  = argparse.ArgumentParser(description = 'Plot')
+    parser.add_argument('-e', '--extract', default = False, action='store_true')
+    parser.add_argument('-n', '--name', help = 'file name')
+    parser.add_argument('-p', '--path')
+
+    if args.extract:
+        extract(args.path, args.name)
+    else:
+        plit(args.name)
     _, path, save_name = sys.argv
-    extract(path, save_name)
