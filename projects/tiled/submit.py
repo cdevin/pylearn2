18f4deb893cc76a93ae9261b5478c0aec24c2728
Mehdi Mirza <memirzamo@gmail.com>
2014-01-17 16:59:20 -0500

    new experiment

--- projects/tiled/submit.py
+++ projects/tiled/submit.py
@@ -151,6 +151,86 @@ def sparse_linear(submit = False, make = False):
     if not make:
         db.createView(TABLE_NAME + '_view')
 
+def sparse_maxout(submit = False, make = False):
+    state = DD()
+    with open('exp/penntree_maxout_local_maxout.yaml') as ymtmp:
+        state.yaml_string = ymtmp.read()
+
+    state.db = 'penntree'
+    state.seq_len = 6
+    state.embed_dim = 96
+    state.img_shape = 24
+    state.l1_num_pieces = 2
+    state.kernel_shape = 2
+    state.l2_units = 50
+    state.l2_pieces = 2
+    state.batch_size = 128
+    state.learning_rate = 0.1
+    state.m_stat = 100
+    state.final_momentum = 0.7
+    state.lr_sat = 100
+    state.decay = 0.1
+    num_exp = 30
+    if submit:
+        TABLE_NAME = "pentree_sparse_local_maxout"
+        db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
+        state.save_path = './'
+    else:
+        state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/penntree_maxout_local_maxout/")
+        PATH = state.save_path
+
+    rng = np.random.RandomState([2014, 1, 15])
+
+    for i in xrange(num_exp):
+        state.h0_col_norm = rng.uniform(1., 2.)
+        state.h1_col_norm = rng.uniform(1., 2.)
+        state.h2_col_norm = rng.uniform(1., 2.)
+        state.h3_col_norm = rng.uniform(2., 4.)
+        state.y_col_norm = rng.uniform(3., 7.)
+
+        state.embed_dim = rng.randint(50, 150)
+        state.img_shape = rng.randint(10, 30)
+        state.l1_units = state.img_shape ** 2
+        state.l1_pieces = rng.randint(2, 5)
+        state.l2_num_pieces = rng.randint(2, 5)
+        state.kernel_shape = rng.randint(2, 6)
+        state.l3_units = rng.randint(50, 200)
+        state.l3_pieces = rng.randint(2, 5)
+        state.learning_rate = 10. ** rng.uniform(1., -2)
+        state.m_sat = rng.randint(2, 200)
+        state.final_momentum = rng.uniform(.5, .7)
+        state.lr_sat =rng.randint(50, 200)
+        state.decay = 10. ** rng.uniform(-3, -1)
+
+        def random_init_string():
+            irange = 10. ** rng.uniform(-2.3, -1.)
+            return "irange: " + str(irange)
+
+        state.h0_init = random_init_string()
+        state.h1_init = random_init_string()
+        state.h2_init = random_init_string()
+        state.h3_init = random_init_string()
+        if rng.randint(2):
+            state.y_init = "sparse_init: 0"
+        else:
+            state.y_init = random_init_string()
+
+        if make:
+            state.save_path = os.path.join(PATH, str(i)) + '/'
+            if not os.path.isdir(state.save_path):
+                os.mkdir(state.save_path)
+            yaml = state.yaml_string % (state)
+            with open(os.path.join(state.save_path, 'model.yaml'), 'w') as fp:
+                fp.write(yaml)
+        else:
+            if submit:
+                sql.insert_job(experiment, flatten(state), db)
+            else:
+                experiment(state, None)
+
+    if not make:
+        db.createView(TABLE_NAME + '_view')
+
 def sparse_linear2(submit = False, make = False):
     """
     2 sparse linear layer
@@ -388,7 +468,7 @@ def dense(submit = False, make = False):
 if __name__ == "__main__":
 
     parser  = argparse.ArgumentParser(description = 'job submitter')
-    parser.add_argument('-t', '--task', choices = ['sparse', 'channel', 'linear', 'linear2', 'dense'])
+    parser.add_argument('-t', '--task', choices = ['sparse', 'channel', 'linear', 'linear2', 'dense', 'maxout'])
     parser.add_argument('-s', '--submit', default = False, action='store_true')
     parser.add_argument('-m', '--make', default = False, action='store_true')
     args = parser.parse_args()
@@ -397,6 +477,8 @@ if __name__ == "__main__":
         sparse(args.submit)
     elif args.task == 'linear':
         sparse_linear(args.submit, args.make)
+    elif args.task == 'maxout':
+        sparse_maxout(args.submit, args.make)
     elif args.task == 'linear2':
         sparse_linear2(args.submit, args.make)
     elif args.task == 'dense':
