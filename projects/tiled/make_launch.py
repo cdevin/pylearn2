da9c23b59a900aaefdc58342d7352925eb4f9aa0
Mehdi Mirza <memirzamo@gmail.com>
2014-01-27 10:42:04 -0500

    new exp

--- projects/tiled/make_launch.py
+++ projects/tiled/make_launch.py
@@ -1,6 +1,7 @@
 base = """jobdispatch --gpu --pre_tasks='export THEANO_FLAGS=floatX=float32,device=gpu${BLOCK_TASK_ID},force_device=True,base_compiledir=/localscratch/$USER/.theano/' --pre_tasks='rm -rf /tmp/$USER' /home/mmirza/projects/noisylearn/projects/tiled/worker.sh %(path)s"{{%(args)s}}\""""
 args = ','.join([str(job_id) for job_id in xrange(30)])
-path = "/gs/scratch/mmirza/results/pentree_dense/"
+#path = "/gs/scratch/mmirza/results/pentree_dense/"
+path = "/gs/scratch/mmirza/results/pentree_sparse_local1/"
 f = open('launch.sh', 'w')
 f.write(base % locals())
 f.close()
--- projects/tiled/submit.py
+++ projects/tiled/submit.py
@@ -73,84 +73,6 @@ def sparse(submit = False):
 
     db.createView(TABLE_NAME + '_view')
 
-def sparse_linear(submit = False, make = False):
-    state = DD()
-    with open('exp/penntree_maxout_local_linear.yaml') as ymtmp:
-        state.yaml_string = ymtmp.read()
-
-    state.db = 'penntree'
-    state.seq_len = 6
-    state.embed_dim = 96
-    state.img_shape = 24
-    state.l1_num_pieces = 2
-    state.kernel_shape = 2
-    state.l2_units = 50
-    state.l2_pieces = 2
-    state.batch_size = 128
-    state.learning_rate = 0.1
-    state.m_stat = 100
-    state.final_momentum = 0.7
-    state.lr_sat = 100
-    state.decay = 0.1
-    num_exp = 25
-    if submit:
-        TABLE_NAME = "pentree_sparse_local_linear"
-        db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
-        state.save_path = './'
-    else:
-        state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/pentree_sparse_local/")
-        PATH = state.save_path
-
-    rng = np.random.RandomState([2014, 1, 15])
-
-    for i in xrange(num_exp):
-        state.h0_col_norm = rng.uniform(1., 3.)
-        state.h1_col_norm = rng.uniform(1., 3.)
-        state.h2_col_norm = rng.uniform(1., 2.)
-        state.h3_col_norm = rng.uniform(2., 5.)
-        state.y_col_norm = rng.uniform(3., 10.)
-
-        state.img_shape = rng.randint(10, 30)
-        state.linear_dim = state.img_shape ** 2
-        state.l2_num_pieces = rng.randint(2, 5)
-        state.kernel_shape = rng.randint(2, 6)
-        state.l3_units = rng.randint(50, 200)
-        state.l3_pieces = rng.randint(2, 5)
-        state.learning_rate = 10. ** rng.uniform(1., -2)
-        state.m_sat = rng.randint(2, 200)
-        state.final_momentum = rng.uniform(.5, .7)
-        state.lr_sat =rng.randint(50, 200)
-        state.decay = 10. ** rng.uniform(-3, -1)
-
-        def random_init_string():
-            irange = 10. ** rng.uniform(-2.3, -1.)
-            return "irange: " + str(irange)
-
-        state.h0_init = random_init_string()
-        state.h1_init = random_init_string()
-        state.h2_init = random_init_string()
-        state.h3_init = random_init_string()
-        if rng.randint(2):
-            state.y_init = "sparse_init: 0"
-        else:
-            state.y_init = random_init_string()
-
-        if make:
-            state.save_path = os.path.join(PATH, str(i)) + '/'
-            if not os.path.isdir(state.save_path):
-                os.mkdir(state.save_path)
-            yaml = state.yaml_string % (state)
-            with open(os.path.join(state.save_path, 'model.yaml'), 'w') as fp:
-                fp.write(yaml)
-        else:
-            if submit:
-                sql.insert_job(experiment, flatten(state), db)
-            else:
-                experiment(state, None)
-
-    if not make:
-        db.createView(TABLE_NAME + '_view')
-
 def sparse_maxout(submit = False, make = False):
     state = DD()
     with open('exp/penntree_maxout_local_maxout.yaml') as ymtmp:
@@ -212,6 +134,90 @@ def sparse_maxout(submit = False, make = False):
         state.h3_init = random_init_string()
         if rng.randint(2):
             state.y_init = "sparse_init: 0"
+        else:
+            state.y_init = random_init_string()
+
+        if make:
+            state.save_path = os.path.join(PATH, str(i)) + '/'
+            if not os.path.isdir(state.save_path):
+                os.mkdir(state.save_path)
+            yaml = state.yaml_string % (state)
+            with open(os.path.join(state.save_path, 'model.yaml'), 'w') as fp:
+                fp.write(yaml)
+        else:
+            if submit:
+                sql.insert_job(experiment, flatten(state), db)
+            else:
+                experiment(state, None)
+
+    if not make:
+        db.createView(TABLE_NAME + '_view')
+
+def sparse_linear(submit = False, make = False):
+    """
+    2 sparse linear layer
+    """
+    state = DD()
+    with open('exp/penntree_maxout_local_linear.yaml') as ymtmp:
+        state.yaml_string = ymtmp.read()
+
+    state.db = 'penntree'
+    state.seq_len = 5
+    state.batch_size = 640
+    state.embed_use_bias = 0
+    state.h1_use_bias = 0
+    state.converter_num_channel = 3
+    num_exp = 20
+    if submit:
+        TABLE_NAME = "pentree_sparse_local_linear2_new_gui"
+        db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
+        state.save_path = './'
+    else:
+        state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/pentree_sparse_local1/")
+        PATH = state.save_path
+
+    rng = np.random.RandomState([2014, 1, 15])
+
+    for i in xrange(num_exp):
+        state.h0_max_col_norm = rng.uniform(1., 2.)
+        #state.h0_max_col_norm = 1.
+        state.h0_min_col_norm = 0
+        state.h1_col_norm = rng.uniform(1., 2.)
+        state.h2_col_norm = rng.uniform(2., 3.5)
+        state.h3_col_norm = rng.uniform(2., 3.5)
+        state.h4_col_norm = rng.uniform(1., 2.5)
+        state.y_col_norm = rng.uniform(3., 8.)
+
+        channel_options = [16]
+        state.h2_channels = channel_options[rng.randint(len(channel_options))]
+        state.h3_channels = channel_options[rng.randint(len(channel_options))]
+
+        state.embed_dim = rng.randint(200, 1000)
+        state.img_shape = rng.randint(10, 40)
+        state.linear_dim = (state.img_shape ** 2) * state.converter_num_channel
+        state.h2_num_pieces = rng.randint(2, 5)
+        state.h2_kernel_shape = rng.randint(2, 5)
+        state.h3_num_pieces = rng.randint(2, 5)
+        state.h3_kernel_shape = rng.randint(2, 4)
+        state.h4_units = rng.randint(100, 1000)
+        state.h4_pieces = rng.randint(2, 5)
+        state.learning_rate = 10. ** rng.uniform(1., -2)
+        state.m_sat = rng.randint(50, 200)
+        state.final_momentum = rng.uniform(.6, .75)
+        state.lr_sat =rng.randint(50, 200)
+        state.decay = 10. ** rng.uniform(-3, -1)
+
+        def random_init_string(low = -2.3, high = -1.):
+            irange = 10. ** rng.uniform(-2.3, -1.)
+            return "irange: " + str(irange)
+
+        state.h0_init = random_init_string(-4, -2)
+        state.h1_init = random_init_string()
+        state.h2_init = random_init_string()
+        state.h3_init = random_init_string()
+        state.h4_init = random_init_string()
+        if rng.randint(2):
+            state.y_init = "sparse_init: {}".format([0, 10, 100][rng.randint(3)])
         else:
             state.y_init = random_init_string()
 
