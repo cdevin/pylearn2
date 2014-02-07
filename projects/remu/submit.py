5ca80646086fd5e20c694243e86a89725d77a192
Mehdi Mirza <memirzamo@gmail.com>
2014-02-07 11:28:44 -0500

    svhn exp ready

--- projects/remu/submit.py
+++ projects/remu/submit.py
@@ -8,59 +8,67 @@ from pylearn2.utils.string_utils import preprocess
 
 DATA_PATH = get_data_path()
 
-def composite_relu(submit = False, make = False):
+def svhn(submit = False, make = False):
     """
-    2 sparse linear composite layer
+    SVHN
     """
     state = DD()
-    with open('exp/penntree_maxout_local_relu_composite.yaml') as ymtmp:
+    with open('exp/svhn.yaml') as ymtmp:
         state.yaml_string = ymtmp.read()
 
-    state.db = 'penntree'
-    state.seq_len = 7
-    state.batch_size = 640
-    state.embed_use_bias = 0
-    state.h1_use_bias = 0
-    state.converter_num_channel = 3
-    num_exp = 20
+    state.db = 'SVHN'
+    state.orig_path = '/RQexec/mirzameh/data/SVHN/'
+    state.data_path = '/tmp/mmirza/SVHN/'
+    num_exp = 25
     if submit:
-        TABLE_NAME = "pentree_sparse_local_relu_composite"
+        TABLE_NAME = "remu_svhn"
         db = api0.open_db("postgres://mirzamom:pishy83@opter.iro.umontreal.ca/mirzamom_db?table=" + TABLE_NAME)
         state.save_path = './'
     else:
-        state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/pentree_sparse_relu_composite_7/")
+        state.save_path = preprocess("${PYLEARN2_EXP_RESULTS}/remu_svhn/")
         PATH = state.save_path
 
     rng = np.random.RandomState([2014, 1, 15])
 
     for i in xrange(num_exp):
-        state.h0_max_col_norm = rng.uniform(1., 4.)
-        #state.h0_max_col_norm = 1.
-        state.h0_min_col_norm = 0
-        state.h1_col_norm = rng.uniform(1., 4.)
-        state.h2_col_norm = rng.uniform(1., 2.5)
-        state.h3_col_norm = rng.uniform(1., 2.5)
-        state.h4_col_norm = rng.uniform(1., 2.5)
-        state.y_col_norm = rng.uniform(1., 3.)
+        state.y_max_col_norm = rng.uniform(1., 3.)
 
-        channel_options = [16]
-        state.h2_channels = channel_options[rng.randint(len(channel_options))]
+        channel_options = [16, 32, 64, 128, 256]
+        state.h0_num_channels = channel_options[rng.randint(len(channel_options))]
+        state.h1_num_channels = channel_options[rng.randint(len(channel_options))]
+        state.h2_num_channels = channel_options[rng.randint(len(channel_options))]
 
-        state.embed_dim = rng.randint(10, 300)
-        state.img_shape = rng.randint(15, 50)
-        state.linear_dim = (state.img_shape ** 2) * state.converter_num_channel
-        state.h2_num_pieces = rng.randint(2, 3)
-        state.h2_kernel_shape = rng.randint(2, 3)
-        state.h3_num_pieces = rng.randint(2, 3)
-        state.h3_units = rng.randint(50, 200)
-        state.h3_pieces = rng.randint(2, 3)
-        state.h4_units = rng.randint(100, 1000)
-        state.h4_pieces = rng.randint(2, 3)
-        state.learning_rate = 10. ** rng.uniform(1., -1)
-        state.m_sat = rng.randint(50, 200)
-        state.final_momentum = rng.uniform(.6, .7)
+        state.h0_num_pieces = rng.randint(2, 5)
+        state.h1_num_pieces = rng.randint(2, 5)
+        state.h2_num_pieces = rng.randint(2, 5)
+
+        state.h0_kernel_shape = rng.randint(2, 8)
+        state.h1_kernel_shape = rng.randint(2, 7)
+        state.h2_kernel_shape = rng.randint(2, 6)
+
+        state.h0_pool_shape = rng.randint(2, 4)
+        state.h1_pool_shape = rng.randint(2, 4)
+        state.h2_pool_shape = rng.randint(2, 4)
+
+        state.h0_pool_stride = rng.randint(1, state.h0_pool_shape)
+        state.h1_pool_stride = rng.randint(1, state.h1_pool_shape)
+        state.h2_pool_stride = rng.randint(1, state.h2_pool_shape)
+
+        state.h3_num_units = rng.randint(100, 1000)
+        state.h3_num_pieces = rng.randint(2, 5)
+        state.h3_w_scale= 1.
+        state.h3_slope_scale = 1.
+
+        state.learning_rate = 10. ** rng.uniform(1., -2)
+        state.momentum_saturate = rng.randint(50, 200)
+        state.final_momentum = rng.uniform(.6, .9)
         state.lr_sat =rng.randint(50, 200)
-        state.decay = 10. ** rng.uniform(-3, -1)
+        state.lr_decay = 10. ** rng.uniform(-3, -1)
+
+        state.h0_islope = 10 ** rng.uniform(0, -2.)
+        state.h1_islope = 10 ** rng.uniform(0, -2.)
+        state.h2_islope = 10 ** rng.uniform(0, -2.)
+        state.h3_islope = 10 ** rng.uniform(0, -2.)
 
         def random_init_string(low = -2.3, high = -1.):
             irange = 10. ** rng.uniform(-2.3, -1.)
@@ -70,7 +78,6 @@ def composite_relu(submit = False, make = False):
         state.h1_init = random_init_string()
         state.h2_init = random_init_string()
         state.h3_init = random_init_string()
-        state.h4_init = random_init_string()
         if rng.randint(2):
             state.y_init = "sparse_init: {}".format([0, 10, 100][rng.randint(3)])
         else:
@@ -95,15 +102,13 @@ def composite_relu(submit = False, make = False):
 if __name__ == "__main__":
 
     parser  = argparse.ArgumentParser(description = 'job submitter')
-    parser.add_argument('-t', '--task', choices = ['sparse',
-            'channel', 'linear', 'linear2', 'dense', 'maxout',
-            'wiki_sparse2', 'composite', 'relu', 'composite_relu'])
+    parser.add_argument('-t', '--task', choices = ['svhn'])
     parser.add_argument('-s', '--submit', default = False, action='store_true')
     parser.add_argument('-m', '--make', default = False, action='store_true')
     args = parser.parse_args()
 
-    if args.task == 'sparse':
-        sparse(args.submit)
+    if args.task == 'svhn':
+        svhn(args.submit)
     else:
         raise ValueError or("Wrong task optipns {}".fromat(args.task))
 
