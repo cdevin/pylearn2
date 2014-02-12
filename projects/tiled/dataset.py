26eea332e9699a0e3a891a978832e53f394b833e
Mehdi Mirza <memirzamo@gmail.com>
2014-02-12 15:58:34 -0500

    wikipedia split in two files

---
+++ projects/tiled/dataset.py
@@ -0,0 +1,116 @@
+import warnings
+import functools
+from pylearn2.datasets import Dataset
+from pylearn2.datasets import dense_design_matrix
+from pylearn2.utils import safe_zip
+from noisylearn.projects.tiled.iteration import FiniteDatasetIterator
+
+
+class SequenceDesignMatrix(dense_design_matrix.DenseDesignMatrix):
+
+    def get_data(self):
+        return self.X, np.zeros(1, dtype='int32')
+
+    @property
+    def num_examples(self):
+        return (self.X.shape[0] - self.seq_len)
+
+
+    @functools.wraps(Dataset.iterator)
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 topo=None, targets=None, rng=None, data_specs=None,
+                 return_tuple=False):
+        """
+        .. todo::
+
+            WRITEME
+        """
+
+        if topo is not None or targets is not None:
+            if data_specs is not None:
+                raise ValueError("In DenseDesignMatrix.iterator, both "
+                        "the `data_specs` argument and deprecated arguments "
+                        "`topo` or `targets` were provided.",
+                        (data_specs, topo, targets))
+
+            warnings.warn("Usage of `topo` and `target` arguments are being "
+                    "deprecated, and will be removed around November 7th, "
+                    "2013. `data_specs` should be used instead.",
+                    stacklevel=2)
+            # build data_specs from topo and targets if needed
+            if topo is None:
+                topo = getattr(self, '_iter_topo', False)
+            if topo:
+                # self.iterator is called without a data_specs, and with
+                # "topo=True", so we use the default topological space
+                # stored in self.X_topo_space
+                assert self.X_topo_space is not None
+                X_space = self.X_topo_space
+            else:
+                X_space = self.X_space
+
+            if targets is None:
+                targets = getattr(self, '_iter_targets', False)
+            if targets:
+                assert self.y is not None
+                y_space = self.data_specs[0].components[1]
+                space = CompositeSpace((X_space, y_space))
+                source = ('features', 'targets')
+            else:
+                space = X_space
+                source = 'features'
+
+            data_specs = (space, source)
+            convert = None
+
+        else:
+            if data_specs is None:
+                data_specs = self._iter_data_specs
+
+            # If there is a view_converter, we have to use it to convert
+            # the stored data for "features" into one that the iterator
+            # can return.
+            space, source = data_specs
+            if isinstance(space, CompositeSpace):
+                sub_spaces = space.components
+                sub_sources = source
+            else:
+                sub_spaces = (space,)
+                sub_sources = (source,)
+
+            convert = []
+            for sp, src in safe_zip(sub_spaces, sub_sources):
+                if (src == 'features' and
+                        getattr(self, 'view_converter', None) is not None):
+                    conv_fn = (lambda batch, self=self, space=sp:
+                               self.view_converter.get_formatted_batch(
+                                   batch,
+                                   space))
+                else:
+                    conv_fn = None
+                convert.append(conv_fn)
+
+        # TODO: Refactor
+        if mode is None:
+            if hasattr(self, '_iter_subset_class'):
+                mode = self._iter_subset_class
+            else:
+                raise ValueError('iteration mode not provided and no default '
+                                 'mode set for %s' % str(self))
+        else:
+            mode = resolve_iterator_class(mode)
+
+        if batch_size is None:
+            batch_size = getattr(self, '_iter_batch_size', None)
+        if num_batches is None:
+            num_batches = getattr(self, '_iter_num_batches', None)
+        if rng is None and mode.stochastic:
+            rng = self.rng
+        return FiniteDatasetIterator(self,
+                                     mode(self.num_examples, batch_size,
+                                     num_batches, rng),
+                                     data_specs=data_specs,
+                                     return_tuple=return_tuple,
+                                     convert=convert)
+
+
