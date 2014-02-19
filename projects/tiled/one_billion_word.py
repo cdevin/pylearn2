c431e106b73f013672f180599dc88ccd48120086
Mehdi Mirza <memirzamo@gmail.com>
2014-02-19 16:23:42 -0500

    added a lyx file

--- projects/tiled/one_billion_word.py
+++ projects/tiled/one_billion_word.py
@@ -1,63 +1,51 @@
 import os
 import numpy as np
-from collections import Counter
-from pylearn2.datasets import dense_design_matrix
-from pylearn2.space import VectorSpace
 from pylearn2.utils import serial
 from pylearn2.utils.string_utils import preprocess
-from noisylearn.projects.tiled.brown_utils import map_words, BrownClusterDict
+from pylearn2.space import VectorSpace, CompositeSpace
+from pylearn2.utils.iteration import resolve_iterator_class
+from noisylearn.projects.tiled.dataset import SequenceDesignMatrix
 
+class OneBilllionWords(SequenceDesignMatrix):
 
-class OneBillionWord(dense_design_matrix.DenseDesignMatrix):
+    _default_seed = (17, 2, 946)
 
     valid_set_names = ["train","valid", "test"]
-    def __init__(self, which_set, seq_len, brown = None, sentence = True):
-
-        if not sentence:
-            raise NotImplementedError("Shuffled wrods version is not implemented")
+    def __init__(self, which_set, seq_len, rng=_default_seed):
 
         if which_set not in self.valid_set_names:
             raise ValueError("which_set should have one of these values: {}".format(self.valid_set_names))
         data = serial.load(os.path.join("${PYLEARN2_DATA_PATH}",
-                "smt/billion/en/newsxx.test.npy"))
+                "smt/billion/en/newsxx.test.npy")
 
-        x = []
-        y = []
-        for item in data:
-        for i in xrange(tot_len):
-            x[i] = data[i:i+seq_len]
-            y[i] = data[i + seq_len]
+        self.seq_len = seq_len
+        self.X = data
 
-        super(PennTree, self).__init__(X = x, y = y)
+        x_space = VectorSpace(dim = seq_len)
+        x_source = 'features'
+        y_space = VectorSpace(dim=1)
+        y_source = 'targets'
 
-        self.brown = brown
-        if brown is not None:
-            cluster_path = "${PYLEARN2_DATA_PATH}/"\
-                    "PennTreebankCorpus/brown-cluster/" + \
-                    "train-c{}-p1.out/paths".format(brown - 1)
-            clusters = BrownClusterDict(preprocess(cluster_path))
-            word_dicts = serial.load(os.path.join("${PYLEARN2_DATA_PATH}",
-                "PennTreebankCorpus/dictionaries.npz"))
-            mapped_dict, clusters = map_words(word_dicts, clusters)
+        space = CompositeSpace((x_space, y_space))
+        source = (x_source, y_source)
+        self.data_specs = (space, source)
+        self.X_space = x_space
 
-            cls = np.zeros((tot_len, 1))
-            for i in xrange(tot_len):
-                cls[i] = clusters[y[i]]
-                y[i] = mapped_dict[y[i]]
-
-            source = ('features', 'targets', 'classes')
-            space = self.data_specs[0]
-            space.components.append(VectorSpace(dim=1))
-            self.data_specs = (space, source)
-            self.cls = cls
-
-
-    def get_data(self):
-        if self.brown is None:
-            return (self.X, self.y)
+        self.compress = False
+        self.design_loc = None
+        if hasattr(rng, 'random_integers'):
+            self.rng = rng
         else:
-            return (self.X, self.y, self.cls)
+            self.rng = np.random.RandomState(rng)
+        # Defaults for iterators
+        self._iter_mode = resolve_iterator_class('sequential')
+        self._iter_topo = False
+        self._iter_targets = False
+        self._iter_data_specs = (self.X_space, 'features')
 
 
 if __name__ == "__main__":
-    dd = PennTree('train', 3, brown = 51)
+
+    train = OneBilllionWords('train', 6)
+    print train.num_examples
+
