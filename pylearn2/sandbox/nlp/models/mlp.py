8ddfcb0c4b4c12eeee9438c6ed61dd928d895226
cdevin <coline.devin@gmail.com>
2014-09-26 14:42:28 -0400

    Hierarchical Softmax fprop

--- pylearn2/sandbox/nlp/models/mlp.py
+++ pylearn2/sandbox/nlp/models/mlp.py
@@ -1,6 +1,7 @@
 """
 Sandbox multilayer perceptron layers for natural language processing (NLP)
 """
+import numpy as np
 import theano.tensor as T
 from theano import config
 
@@ -213,3 +214,163 @@ class NoisySoftmax(Softmax):
     def cost_matrix(self, Y, Y_hat):
         log_prob_of = self._cost(Y, Y_hat)
         return -log_prob_of
+
+class HierarchicalSoftmax(Layer):
+    """
+    A GPU implementation of the hierarchichal softmax. Uses a two level tree.
+
+    The term 'word' refers to the leaves of the tree and 'group'
+    refers to the non-leaf nodes of the tree. The term 'class' will not
+    be used because in the plain Softmax layer, class refers to the number
+    of outputs, but in many hierarchical softmax implementations, class
+    refers to the number of non-leaf nodes.
+
+
+    Parameters
+    ----------
+    n_words : int
+        Number of possible outputs for softmax targets.
+    layer_name : string
+        Name of Softmax layers.
+    irange : float
+        If specified, initialized each weight randomly in 
+        U(-irange, irange).
+    istdev : float
+        If specified, initialize each weight randomly from
+        N(0,istdev).
+    no_affine : boolean
+        If True, softmax nonlinearity is applied directly to
+        inputs.
+    binary_target_dim : int, optional
+        If your targets are class labels (i.e. a binary vector) then set the
+        number of targets here so that an IndexSpace of the proper dimension
+        can be used as the target space. This allows the softmax to compute
+        the cost much more quickly than if it needs to convert the targets
+        into a VectorSpace.
+    full_softmax: bool,  defaults to false
+        Set to true if you want to calculate the probablility of every word.
+    """
+
+    def __init__(self, n_words, layer_name, irange=None,
+                 istdev=None, no_affine=False,
+                 binary_target_dim=None, full_softmax=False):
+
+        super(Softmax, self).__init__()
+
+        self.__dict__.update(locals())
+        del self.self
+        del self.init_bias_target_marginals
+
+        assert isinstance(n_classes, py_integer_types)
+
+        if binary_target_dim is not None:
+            assert isinstance(binary_target_dim, py_integer_types)
+            self._has_binary_target = True
+            self._target_space = IndexSpace(dim=binary_target_dim, 
+                                            max_labels=n_classes)
+        else:
+            self._has_binary_target = False
+    
+        self.output_space = VectorSpace(n_classes)
+
+    @wraps(Layer.set_input_space)
+    def set_input_space(self, space):
+        self.input_space = space
+
+        if not isinstance(space, Space):
+            raise TypeError("Expected Space, got " +
+                            str(space)+" of type "+str(type(space)))
+
+        self.input_dim = space.get_total_dimension()
+        self.needs_reformat = not isinstance(space, VectorSpace)
+
+        if self.no_affine:
+            desired_dim = self.n_words
+            assert self.input_dim == desired_dim
+        else:
+            desired_dim = self.input_dim
+        self.desired_space = VectorSpace(desired_dim)
+
+        if not self.needs_reformat:
+            assert self.desired_space == self.input_space
+
+        rng = self.mlp.rng
+        self.group_size = np.ceil(np.sqrt(self.n_words)).astype('int64')
+        self.n_groups = np.ceil(self.n_words/float(self.group_size)).astype('int64')
+        self.iBlocks = 1  # number of blocks in the input (from lower layer)
+
+        if self.no_affine:
+            self._params = []
+        else:
+            if self.irange is not None:
+                assert self.istdev is None
+                W1 = rng.uniform(-self.irange,
+                                 self.irange,
+                                 (self.input_dim, self.n_groups))
+
+                # Has an input-> word matrix for each group
+                W2 = rng.uniform(-self.irange,
+                                 self.irange, 
+                                 (self.iBlocks, self.n_groups, 
+                                  self.input_dim, self.group_size))
+            elif self.istdev is not None:
+                W1 = rng.randn(self.input_dim, self.n_groups) * self.istdev
+                W2 = rng.randn(self.iBlocks, self.n_groups, self.input_dim, 
+                               self.group_size) * self.istdev
+
+            self.W1 = sharedX(W1,  '%s_W1'%self.layer_name)
+            self.b1 = sharedX(np.zeros((self.n_groups,)), name='%s_b1'%self.layer_name)
+            self.W2 = sharedX(W2,  '%s_W2'%self.layer_name)
+            self.b1 = sharedX(np.zeros((self.n_groups, self.group_size)), 
+                              name='%s_b1'%self.layer_name)
+
+            self._params = [self.b1, self.W1, self.b2, self.W2]
+            
+
+    @wraps(Layer.fprop)
+    def fprop(self, state_below):
+        self.input_space.validate(state_below)
+        if self.needs_reformat:
+            state_below = self.input_space.format_as(state_below,
+                                                     self.desired_space)
+
+        self.desired_space.validate(state_below)
+        assert state_below.ndim == 2
+
+        if not hasattr(self, 'no_affine'):
+            self.no_affine = False
+
+        # if self.no_affine:
+        #     group_vals = state_below;
+        # else:
+        #     assert self.W1.ndim == 2
+        #     group_vals = T.dot(state_below, self.W1) + b1
+
+
+        if self.full_softmax:
+            group_vals = T.dot(state_below, self.W1) + b1
+            groups_vec = T.arange(self.n_groups)
+            
+            
+            def _compute_ingroup(group_index):
+                word_vals = T.nnet.softmax(
+                    TT.dot(state_below, self.W2[0, group_index, :, :])
+                    +self.b2[group_index,:])
+                weighted_word_val = word_val * group_vals[:, group_index][:,None]
+                return weighted_word_val.T
+
+            val = theano.scan(_compute_ingroup, 
+                              groups_vec, 
+                              None, 
+                              name='compute_ingroup')
+            print val.ndim
+            all_word_val = val[0].reshape([val[0].shape[0]*val[0].shape[1], val[0].shape[2]]).T
+            all_word_val = all_word_val[:,:self.n_out]
+            emb_val = all_word_val
+
+        for value in get_debug_values(rval):
+            if self.mlp.batch_size is not None:
+                assert value.shape[0] == self.mlp.batch_size
+
+        return rval
+
